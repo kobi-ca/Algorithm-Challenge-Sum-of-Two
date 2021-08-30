8b931d0f4afd4478748e2a7a6919ed04283ebd48
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2021-08-29 18:50:34 -0700

    Initial revision
    Python code working

---
+++ README.md
@@ -0,0 +1,1 @@
+https://medium.com/code-85/algorithm-challenge-sum-of-two-3de11858799e
---
+++ python/solution.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+def sum_of_two_initial(list1: List, list2: List, v):
+    for a in list1:
+        for b in list2:
+            if a + b == v:
+                return True
+    return False
+
+
+def sum_of_two_optimized(list1: List, list2: List, v):
+    answers = set()
+    for a in list1:
+        answers.add(v - a)
+    for b in list2:
+        if b in answers:
+            return True
+    return False
+
+
+def run_solution():
+    list1 = [1, 2, 3, 4]
+    list2 = [0, 0, - 1, 4]
+
+    # returning true
+    result = sum_of_two_initial(list1, list2, 8)  # 4 + 4
+    print(f"Result non optimized {result}")
+    result = sum_of_two_optimized(list1, list2, 8)  # 4 + 4
+    print(f"Result optimized {result}")
+
+    # returning false
+    list1 = [1, 2, 3, 4]
+    list2 = [0, 0, -1, 4]
+    result = sum_of_two_optimized(list1, list2, 9)
+    print(f"Result optimized {result}")
+
+
+if __name__ == "__main__":
+    run_solution()
